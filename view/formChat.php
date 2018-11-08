396a4a3804bde58b4e0907aa3fdc646b72913d31
André <andrelokal@gmail.com>
2018-11-08 19:37:10 -0200

    Update formChat.php

--- view/formChat.php
+++ view/formChat.php
@@ -3,17 +3,6 @@
 include_once('util/config.php');
 include_once('model/Emogi.php');
 ?>
-<script>
-    //Google Analiticts
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-97592513-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
 <!DOCTYPE html>
 <html>
 <head>
