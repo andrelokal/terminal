fab7a91a188d8c995b22adec78cc48f229fbb4e9
André <andrelokal@gmail.com>
2018-11-08 19:30:42 -0200

    Update formLogin.php

--- view/formLogin.php
+++ view/formLogin.php
@@ -1,18 +1,10 @@
-<script>
-    //Google Analiticts
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-97592513-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
 <form action="control/usuarioController.php" method="post">
-    <label><span>Nome:</span><input type="text" name="nome" /></label>
-    <label><span>Sala:</span><input type="text" name="sala" /></label>
-    <label><span>Senha:</span><input type="password" name="senha" /></label>
+    <label><span>Perfil:</span></label><select name="nome">
+                                    <option value="Cliente">Cliente</option>
+                                    <option value="Balcao">Balcao</option>
+                                </select><!-- input type="text" name="nome" /-->
+    <!--label><span>Sala:</span><input type="text" name="sala" /></label-->
+    <label><span>Loja: </span><input type="tel" name="senha" /></label>
     <label>Cor:</label><select name="cor">
             <option value="#000000">Preto</option>
             <option value="#0000CD" style="color: #0000CD;">Azul</option>
@@ -24,5 +16,6 @@
             <option value="#9400D3" style="color: #9400D3;">Roxo</option>
         </select></br></br>
     <input type="hidden" name="action" value="1" />
+    <input type="hidden" name="sala" value="loja" />
     <input type="submit" name="submit" value="Entrar" />
 </form>
