c7adb8573509874db03abc44924ecfbf77509df1
André <andrelokal@gmail.com>
2018-11-08 19:31:51 -0200

    Update index.php

--- index.php
+++ index.php
@@ -2,26 +2,6 @@
 include_once('util/config.php');
 
 ?>
-
-    <script>
-        //Google Analiticts
-        (function (i, s, o, g, r, a, m) {
-            i['GoogleAnalyticsObject'] = r;
-            i[r] = i[r] || function () {
-                (i[r].q = i[r].q || []).push(arguments)
-            }, i[r].l = 1 * new Date();
-            a = s.createElement(o),
-                m = s.getElementsByTagName(o)[0];
-            a.async = 1;
-            a.src = g;
-            m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-        ga('create', 'UA-97592513-1', 'auto');
-        ga('send', 'pageview');
-
-    </script>
-
     <link href="chat.css?1" rel="stylesheet" type="text/css"/>
 
 <?php
